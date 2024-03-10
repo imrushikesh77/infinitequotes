a3ed94a23a269cae70b6c06c804283e0034d1a5a
imrushikesh77 <rushikeshjawale2004@gmail.com>
2024-03-10 22:32:39 +0530

    shimmer updated

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ const App = () => {
             <Shimmar/>
             <Shimmar/>
             <Shimmar/>
+            <Shimmar/>
+            <Shimmar/>
         </div>
          : (
           <InfiniteScroll 
